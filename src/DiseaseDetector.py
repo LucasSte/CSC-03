cc4a6abd5ee75a996f3a0764fc6a01bc0ea73bd2
vidalmatheus <matheusvidaldemenezes@gmail.com>
2020-07-02 16:33:47 -0300

    Fixing threads for detection

--- src/DiseaseDetector.py
+++ src/DiseaseDetector.py
@@ -1,3 +1,5 @@
+import tensorflow as tf
+from keras.backend import clear_session, set_session
 from keras.models import load_model
 import numpy as np
 import pathlib
@@ -6,14 +8,21 @@ import os
 
 class DiseaseDetector:
     model = None
-
     '''
         Instantiate this class only once, as the constructor is slow
     '''
     def __init__(self):
+        global graph
+        global sess
         os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
         path = str(pathlib.Path(__file__).parent.absolute())
+        # self.model._make_predict_function()
+        sess = tf.Session()
+        graph = tf.get_default_graph()
+        set_session(sess)
         self.model = load_model(path + "/TreeColor.hdf5")
+        
+        
 
     def sigmoid(self, x):
         return 1 / (1 + np.exp(-x))
@@ -26,9 +35,11 @@ class DiseaseDetector:
         Returns true if the plant is sick
     '''
     def detect(self, image):
+        
         shape = image.shape
-
+    
         if shape[0] < 128 or shape[1] < 128:
+            print("Image shape less than 128")
             return False
 
         if shape[0] > 128:
@@ -42,18 +53,24 @@ class DiseaseDetector:
             yf = middle_y + 64
 
             image = image[xi:xf, yi:yf]
-
+        
         image = np.array(image)
         image = image.astype(float)
         image = image / 255
         image = np.reshape(image, (1, 128, 128, 3))
 
-        pred = self.model.predict(image)
-        pred = np.reshape(pred, 1)
-        pred = self.sigmoid(pred)
+        global sess
+        global graph
+        with graph.as_default():
+            set_session(sess)
+            pred = self.model.predict(image)
+            pred = np.reshape(pred, 1)
+            pred = self.sigmoid(pred)
+            
+            print("Prediction: ", pred)
 
-        if pred > 0.9:
-            return True
+            if pred > 0.9:
+                return True
 
-        else:
-            return False
+            else:
+                return False
